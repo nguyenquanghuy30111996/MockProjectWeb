dd66fb31c11d06049c82b370f1e896e8d82a884d
huyde <huyde@DESKTOP-PL3MEVV>
2022-06-12 23:31:00 +0700

    init project

---
+++ src/test/java/com/vn/MockProjectWeb/MockProjectWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.MockProjectWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockProjectWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
